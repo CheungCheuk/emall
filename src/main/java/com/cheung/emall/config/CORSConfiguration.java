8c5a9cd4ea9c0a006387cb040d2b40cbadd97ad1
CheungCheuk <mp.butterfly@outlook.com>
2019-01-08 16:47:26 +0800

    补充文件

--- src/main/java/com/cheung/emall/config/CORSConfiguration.java
+++ src/main/java/com/cheung/emall/config/CORSConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 /**
  * 允许跨域请求：
+ *
  * 允许浏览器向跨源服务器，发出XMLHttpRequest请求，从而克服了AJAX只能同源使用的限制
  * 当一个资源从与该资源本身所在的服务器不同的域、协议或端口请求一个资源时，资源会发起一个跨域 HTTP 请求
  * 出于安全原因，浏览器限制从脚本内发起的跨源HTTP请求，例如 XMLHttpRequest
@@ -12,7 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * CORS需要浏览器和服务器同时支持
  */
 
-//  允许所有请求都跨域：危险吗？
+//  允许所有请求都跨域：(危险？)
 public class CORSConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
---
+++ src/main/webapp/js/admin.js
@@ -0,0 +1,113 @@
+/**
+ * 判断表单是否为空 
+ * */
+function checkEmpty(value, content){
+    if(value == null || value.length == 0){
+        alert(content+"不能为空");
+        return false;
+    }
+    return true;
+}
+function getUrlParms(para) {
+    //  页面URL的查询部分字符串
+    var queryString = location.search;
+
+    //  数组元素存储包含 参数名和参数值的字符串，“key=value"
+    var arrayPara = new Array();
+
+    //  存储查找到的参数值
+    var arrayValue = new Array();
+
+    if( queryString!=""){
+        var index = 0;
+        //  去除查询匹配 “？“
+        queryString = queryString.substr(1);
+        //  根据 “&” 分割获取多个参数
+        arrayPara = queryString.split("&");
+
+        for (const i in arrayPara) {
+            var paraPrefix = para+"=";
+            if(arrayPara[i].indexOf(paraPrefix)==0 
+            && paraPrefix.length<arrayPara[i].length){
+                //  解码URI
+                arrayValue[index]==decodeURI(arrayPara[i].substr(paraPrefix.lenght))
+                index++;
+            }
+        }
+    }
+    if(arrayValue.length==1){
+        return arrayValue[0];
+    }else if (arrayValue.length==0) {
+        return null
+    }else {
+        return arrayValue;
+    }
+}
+
+/**
+ * 判断是否数字
+ */
+function checkNumber(value,content){
+    if(value.length == 0){
+        alert(content+"不能为空");
+        return false;
+    }
+    if(isNaN(value)){
+        alert(content+"必须输入数字");
+        return false;
+    }
+    return true;
+}
+
+/**
+ * 判断是否是整数
+ */
+function checkInt(value,content){
+    if(value.length == 0){
+        alert(content+"不能为空");
+        return false;
+    }
+    if(isNaN(value)){
+        alert(content+"必须输入整数");
+        return false;
+    }
+    return true;
+}
+
+/**
+ * 判断是否删除
+ */
+function checkDeleteLink(){
+    var  confirmDelete = confirm("确认要删除");
+    if(confirmDelete){return true;}
+    return false;
+}
+
+
+/**
+ * 页面分页跳转
+ * 跳转到第一页、最后一页或者前一页、后一页
+ */
+function jump(page,vue){
+    if('first'==page && ! vue.pagination.first){
+        vue.list(0);
+    }
+    else if ('previous' == page && vue.pagination.hasPrevious) {
+        vue.list(vue.pagination.number-1);
+    }
+    else if ('next' == page && vue.pagination.hasNext) {
+        vue.list(vue.vue.pagination.number+1);
+    }
+    else if ('last' == page && vue.pagination.last){
+        vue.list(vue.pagination.totalPages-1);
+    }
+}
+
+/**
+ * 跳转到指定页面
+ */
+function jumpByNumber(start, vue){
+    if(start !== vue.pagination.number){
+        vue.list(start);
+    }
+}
