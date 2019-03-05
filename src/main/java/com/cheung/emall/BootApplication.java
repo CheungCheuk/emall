e2d33a6ff50157fdc65582ead56dc7b8ac2907bf
CheungCheuk <mp.butterfly@outlook.com>
2019-03-05 21:16:57 +0800

    dao 层注释

--- src/main/java/com/cheung/emall/BootApplication.java
+++ src/main/java/com/cheung/emall/BootApplication.java
@@ -4,12 +4,16 @@ package com.cheung.emall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 // import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 //  @SpringBootApplication = @Configuration + @EnableAutoConfiguration + @ComponentScan
 
 // @EnableCaching  //  启动缓存
 @SpringBootApplication
+@EnableJpaRepositories( basePackages = {"com.chueng.emall.dao", "com.cheung.emall.pojo"})
+@EnableElasticsearchRepositories( basePackages = "com.chueng.emall.es")
 public class BootApplication {
     public static void main(String[] args) {
         //  没有该类的引用实例时，使用.class获取类运行时的有关信息
